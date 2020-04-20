5ed4128dab2f8739886aba07324e7bcf89cb8c6c
martintomazic <Martin.tomazic97@gmail.com>
2020-04-20 15:36:38 +0200

    first commit

---
+++ src/main/java/com/tomazicmartin/countriespopulation/CountriesPopulationApplication.java
@@ -0,0 +1,13 @@
+package com.tomazicmartin.countriespopulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesPopulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountriesPopulationApplication.class, args);
+	}
+
+}
